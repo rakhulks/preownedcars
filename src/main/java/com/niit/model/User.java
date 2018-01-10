a28da0ff6fb9bf6fb39a9d74997591c671e6b10f
rakhulks <rakhul@live.com>
2018-01-10 12:38:47 +0530

    add User class
    Signed-off-by: rakhulks <rakhul@live.com>

---
+++ src/main/java/com/niit/model/User.java
@@ -0,0 +1,13 @@
+package com.niit.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Table(name = "user")
+@Entity
+@Component
+public class User {
+
+}
